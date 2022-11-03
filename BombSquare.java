6be3c678773cbcc6c1a4c469c17775e33c5151f4
Yudai <yudaimadeinjapan@gmail.com>
2022-11-03 15:47:49 +0000

    imported files from moodle

---
+++ BombSquare.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class BombSquare extends GameSquare
+{
+	private boolean thisSquareHasBomb = false;
+	public static final int MINE_PROBABILITY = 10;
+
+	public BombSquare(int x, int y, GameBoard board)
+	{
+		super(x, y, "images/blank.png", board);
+
+		Random r = new Random();
+		thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
+	}
+
+	public void clicked()
+	{
+	}
+}
