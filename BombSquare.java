1fff86e80b6eb3ffeefc3dd45078a6d2992d8822
Yudai <yudaimadeinjapan@gmail.com>
2022-11-03 18:21:08 +0000

    added surroundingBombChecker func to show how many bombs are hidden around a square

--- BombSquare.java
+++ BombSquare.java
@@ -5,6 +5,10 @@ public class BombSquare extends GameSquare
 	private boolean thisSquareHasBomb = false;
 	public static final int MINE_PROBABILITY = 10;
 
+	// store the index of squares with bomb
+	static volatile int[][] arr = new int[10][10];
+
+
 	public BombSquare(int x, int y, GameBoard board)
 	{
 		super(x, y, "images/blank.png", board);
@@ -12,6 +16,12 @@ public class BombSquare extends GameSquare
 		Random r = new Random();
 		thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
 
+		if(thisSquareHasBomb){
+			arr[x][y] = 1;
+		}
+		else{
+			arr[x][y] = 0;
+		}
 	}
 
 	public void clicked()
@@ -19,5 +29,54 @@ public class BombSquare extends GameSquare
 		if (thisSquareHasBomb == true){
 			setImage("images/bomb.png");
 		}
+
+		if (thisSquareHasBomb == false){
+			switch (surroundingBombChecker()) {
+				case 0:
+					setImage("images/0.png");
+					break;
+				case 1:
+					setImage("images/1.png");
+					break;
+				case 2:
+					setImage("images/2.png");
+					break;
+				case 3:
+					setImage("images/3.png");
+					break;
+				case 4:
+					setImage("images/4.png");
+					break;
+				case 5:
+					setImage("images/5.png");
+					break;
+				case 6:
+					setImage("images/6.png");
+					break;
+				case 7:
+					setImage("images/7.png");
+					break;
+				case 8:
+					setImage("images/8.png");
+					break;
+				
+			}
+		}
 	}
+
+	public int surroundingBombChecker(){
+		int counter = 0;
+		arr[this.xLocation][this.yLocation] = 0;
+		for (int i=0; i<3; i++){
+			for (int j=0; j<3; j++){
+				if (0 <= this.xLocation+i-1 && this.xLocation+i-1 <= 9 && 0 <= this.yLocation+j-1 && this.yLocation+j-1 <= 9){
+					if (arr[this.xLocation+i-1][this.yLocation+j-1] == 1){
+						counter++;
+					}
+				}			
+			}
+		}	
+		return counter;
+	}
+	
 }
